71a4a12e809bc54825f8637abc662ca790c0ba26
gabozv <GABRIEL.ZUNIGAVEGA@ucr.ac.cr>
2025-04-01 20:05:08 -0600

    Lab3 Completado

---
+++ Laboratorio3/frontend-lab/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <ListaPaises />
+</template>
+
+<script>
+import ListaPaises from './components/ListaPaises.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ListaPaises
+  }
+};
+</script>
+
+<style></style>
