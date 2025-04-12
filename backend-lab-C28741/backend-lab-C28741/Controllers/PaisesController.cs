21f55e77002391ffde75c0c1622e752f2e109493
gabozv <GABRIEL.ZUNIGAVEGA@ucr.ac.cr>
2025-04-12 12:27:38 -0600

    Laboratorio 4 completado

---
+++ backend-lab-C28741/backend-lab-C28741/Controllers/PaisesController.cs
@@ -0,0 +1,26 @@
+﻿using backend_lab_C28741.Handlers;
+using backend_lab_C28741.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_C28741.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+       private readonly PaisesHandler _paisesHandler;
+        
+        public PaisesController()
+        {
+            _paisesHandler = new PaisesHandler();
+        }
+
+        [HttpGet]
+        public List<PaisModel> Get()
+        {
+            var paises = _paisesHandler.ObtenerPaises();
+            return paises;
+        }
+    }
+}
